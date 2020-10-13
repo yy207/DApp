9e3b37d1d4339df30f1c5a0a150350de7c0a0dac
Yning2333 <1939732643@qq.com>
2020-10-13 18:19:15 +0800

    init

---
+++ pages/cart/cart.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		this is cart page 
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/my/my.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		this is my 
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
